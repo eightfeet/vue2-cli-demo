86d4370d475bf679e244306e2b2ec3b0931ec81c
eightfeet <bajiao000000@163.com>
2016-10-27 00:45:15 +0800

    Update apidata.js

--- src/components/scssapi/apidata.js
+++ src/components/scssapi/apidata.js
@@ -1,6 +1,6 @@
 const data = [
   {
-    variable: [
+    variable_color: [
       {
         name: '$gray-0',
         type: 'color'
@@ -26,6 +26,28 @@ const data = [
         type: 'color'
       },
       {
+        name: '$blue-0',
+        type: 'color'
+      },
+      {
+        name: '$blue-1',
+        type: 'color'
+      },
+      {
+        name: '$green-0',
+        type: 'color'
+      },
+      {
+        name: '$orange-0',
+        type: 'color'
+      },
+      {
+        name: '$red-0',
+        type: 'color'
+      }
+    ],
+    variable_size: [
+      {
         name: '$fz-s',
         type: 'size, 20px .833rem (20px)'
       },
@@ -48,13 +70,15 @@ const data = [
       {
         name: '$fz-ll',
         type: 'size, 36px 1.5rem (36px)'
-      },
+      }
+    ],
+    variable_border_radius: [
       {
         name: '$radius',
         type: 'radius, 10/24rem'
       }
     ],
-    style: [
+    style_font_size: [
       {
         name: '.fz-s',
         type: 'font-size:.833rem (20px)'
@@ -82,7 +106,9 @@ const data = [
       {
         name: '.fz-ll',
         type: 'font-size:1.5rem (36px)'
-      },
+      }
+    ],
+    style_font_color: [
       {
         name: '.gray-0',
         type: 'color'
@@ -106,9 +132,386 @@ const data = [
       {
         name: '.gray-5',
         type: 'color'
+      },
+      {
+        name: '.white-0',
+        type: 'color'
+      },{
+        name: '.blue-0',
+        type: 'color'
+      },
+      {
+        name: '.blue-1',
+        type: 'color'
+      },
+      {
+        name: '.green-0',
+        type: 'color'
+      },
+      {
+        name: '.orange-0',
+        type: 'color'
+      },
+      {
+        name: '.red-0',
+        type: 'color'
       }
+    ],
+    style_background_color: [
+    	{
+        name: '.bg-gray-0',
+        type: 'color'
+      },
+      {
+        name: '.bg-gray-1',
+        type: 'color'
+      },
+      {
+        name: '.bg-gray-2',
+        type: 'color'
+      },
+      {
+        name: '.bg-gray-3',
+        type: 'color'
+      },
+      {
+        name: '.bg-gray-4',
+        type: 'color'
+      },
+      {
+        name: '.bg-gray-5',
+        type: 'color'
+      },
+    	{
+        name: '.bg-white-0',
+        type: 'color'
+      },
+      {
+        name: '.bg-blue-0',
+        type: 'color'
+      },
+      {
+        name: '.bg-blue-1',
+        type: 'color'
+      },
+      {
+        name: '.bg-green-0',
+        type: 'color'
+      },
+      {
+        name: '.bg-orange-0',
+        type: 'color'
+      },
+      {
+        name: '.bg-red-0',
+        type: 'color'
+      }
+    ],
+    style_padding: [
+    	{name: '.pd-1', type: 'padding:.1rem '},
+			{name: '.pd-2', type: 'padding:.2rem '},
+			{name: '.pd-3', type: 'padding:.3rem '},
+			{name: '.pd-4', type: 'padding:.4rem '},
+			{name: '.pd-5', type: 'padding:.5rem '},
+			{name: '.pd-6', type: 'padding:.6rem '},
+			{name: '.pd-7', type: 'padding:.7rem '},
+			{name: '.pd-8', type: 'padding:.8rem '},
+			{name: '.pd-9', type: 'padding:.9rem '},
+			{name: '.pd1', type: 'padding:1rem '},
+			{name: '.pd1-5', type: 'padding:1.5rem '},
+			{name: '.pd2', type: 'padding:2rem '},
+			{name: '.pd2-5', type: 'padding:2.5rem '},
+			{name: '.pd3', type: 'padding:3rem '},
+			{name: '.pd3-5', type: 'padding:3.5rem '},
+			{name: '.pd4', type: 'padding:4rem '},
+			{name: '.pd4-5', type: 'padding:4.5rem '},
+			{name: '.pd5', type: 'padding:5rem '},
+			{name: '.pd5-5', type: 'padding:5.5rem '},
+			{name: '.pd6', type: 'padding:6rem '},
+			{name: '.pd6-5', type: 'padding:6.5rem '},
+			{name: '.pd7', type: 'padding:7rem '},
+			{name: '.pd7-5', type: 'padding:7.5rem '},
+			{name: '.pd8', type: 'padding:8rem '},
+			{name: '.pd8-5', type: 'padding:8.5rem '},
+			{name: '.pd9', type: 'padding:9rem '},
+			{name: '.pd9-5', type: 'padding:9.5rem '},
+
+			{name: '.pdb-1', type: 'padding-bottom:.1rem '},
+			{name: '.pdb-2', type: 'padding-bottom:.2rem '},
+			{name: '.pdb-3', type: 'padding-bottom:.3rem '},
+			{name: '.pdb-4', type: 'padding-bottom:.4rem '},
+			{name: '.pdb-5', type: 'padding-bottom:.5rem '},
+			{name: '.pdb-6', type: 'padding-bottom:.6rem '},
+			{name: '.pdb-7', type: 'padding-bottom:.7rem '},
+			{name: '.pdb-8', type: 'padding-bottom:.8rem '},
+			{name: '.pdb-9', type: 'padding-bottom:.9rem '},
+			{name: '.pdb1', type: 'padding-bottom:1rem '},
+			{name: '.pdb1-5', type: 'padding-bottom:1.5rem '},
+			{name: '.pdb2', type: 'padding-bottom:2rem '},
+			{name: '.pdb2-5', type: 'padding-bottom:2.5rem '},
+			{name: '.pdb3', type: 'padding-bottom:3rem '},
+			{name: '.pdb3-5', type: 'padding-bottom:3.5rem '},
+			{name: '.pdb4', type: 'padding-bottom:4rem '},
+			{name: '.pdb4-5', type: 'padding-bottom:4.5rem '},
+			{name: '.pdb5', type: 'padding-bottom:5rem '},
+			{name: '.pdb5-5', type: 'padding-bottom:5.5rem '},
+			{name: '.pdb6', type: 'padding-bottom:6rem '},
+			{name: '.pdb6-5', type: 'padding-bottom:6.5rem '},
+			{name: '.pdb7', type: 'padding-bottom:7rem '},
+			{name: '.pdb7-5', type: 'padding-bottom:7.5rem '},
+			{name: '.pdb8', type: 'padding-bottom:8rem '},
+			{name: '.pdb8-5', type: 'padding-bottom:8.5rem '},
+			{name: '.pdb9', type: 'padding-bottom:9rem '},
+			{name: '.pdb9-5', type: 'padding-bottom:9.5rem '},
+
+			{name: '.pdt-1', type: 'padding-top:.1rem '},
+			{name: '.pdt-2', type: 'padding-top:.2rem '},
+			{name: '.pdt-3', type: 'padding-top:.3rem '},
+			{name: '.pdt-4', type: 'padding-top:.4rem '},
+			{name: '.pdt-5', type: 'padding-top:.5rem '},
+			{name: '.pdt-6', type: 'padding-top:.6rem '},
+			{name: '.pdt-7', type: 'padding-top:.7rem '},
+			{name: '.pdt-8', type: 'padding-top:.8rem '},
+			{name: '.pdt-9', type: 'padding-top:.9rem '},
+			{name: '.pdt1', type: 'padding-top:1rem '},
+			{name: '.pdt1-5', type: 'padding-top:1.5rem '},
+			{name: '.pdt2', type: 'padding-top:2rem '},
+			{name: '.pdt2-5', type: 'padding-top:2.5rem '},
+			{name: '.pdt3', type: 'padding-top:3rem '},
+			{name: '.pdt3-5', type: 'padding-top:3.5rem '},
+			{name: '.pdt4', type: 'padding-top:4rem '},
+			{name: '.pdt4-5', type: 'padding-top:4.5rem '},
+			{name: '.pdt5', type: 'padding-top:5rem '},
+			{name: '.pdt5-5', type: 'padding-top:5.5rem '},
+			{name: '.pdt6', type: 'padding-top:6rem '},
+			{name: '.pdt6-5', type: 'padding-top:6.5rem '},
+			{name: '.pdt7', type: 'padding-top:7rem '},
+			{name: '.pdt7-5', type: 'padding-top:7.5rem '},
+			{name: '.pdt8', type: 'padding-top:8rem '},
+			{name: '.pdt8-5', type: 'padding-top:8.5rem '},
+			{name: '.pdt9', type: 'padding-top:9rem '},
+			{name: '.pdt9-5', type: 'padding-top:9.5rem '},
+
+			{name: '.pdl-1', type: 'padding-left:.1rem '},
+			{name: '.pdl-2', type: 'padding-left:.2rem '},
+			{name: '.pdl-3', type: 'padding-left:.3rem '},
+			{name: '.pdl-4', type: 'padding-left:.4rem '},
+			{name: '.pdl-5', type: 'padding-left:.5rem '},
+			{name: '.pdl-6', type: 'padding-left:.6rem '},
+			{name: '.pdl-7', type: 'padding-left:.7rem '},
+			{name: '.pdl-8', type: 'padding-left:.8rem '},
+			{name: '.pdl-9', type: 'padding-left:.9rem '},
+			{name: '.pdl1', type: 'padding-left:1rem '},
+			{name: '.pdl1-5', type: 'padding-left:1.5rem '},
+			{name: '.pdl2', type: 'padding-left:2rem '},
+			{name: '.pdl2-5', type: 'padding-left:2.5rem '},
+			{name: '.pdl3', type: 'padding-left:3rem '},
+			{name: '.pdl3-5', type: 'padding-left:3.5rem '},
+			{name: '.pdl4', type: 'padding-left:4rem '},
+			{name: '.pdl4-5', type: 'padding-left:4.5rem '},
+			{name: '.pdl5', type: 'padding-left:5rem '},
+			{name: '.pdl5-5', type: 'padding-left:5.5rem '},
+			{name: '.pdl6', type: 'padding-left:6rem '},
+			{name: '.pdl6-5', type: 'padding-left:6.5rem '},
+			{name: '.pdl7', type: 'padding-left:7rem '},
+			{name: '.pdl7-5', type: 'padding-left:7.5rem '},
+			{name: '.pdl8', type: 'padding-left:8rem '},
+			{name: '.pdl8-5', type: 'padding-left:8.5rem '},
+			{name: '.pdl9', type: 'padding-left:9rem '},
+			{name: '.pdl9-5', type: 'padding-left:9.5rem '},
+
+			{name: '.pdr-1', type: 'padding-right:.1rem '},
+			{name: '.pdr-2', type: 'padding-right:.2rem '},
+			{name: '.pdr-3', type: 'padding-right:.3rem '},
+			{name: '.pdr-4', type: 'padding-right:.4rem '},
+			{name: '.pdr-5', type: 'padding-right:.5rem '},
+			{name: '.pdr-6', type: 'padding-right:.6rem '},
+			{name: '.pdr-7', type: 'padding-right:.7rem '},
+			{name: '.pdr-8', type: 'padding-right:.8rem '},
+			{name: '.pdr-9', type: 'padding-right:.9rem '},
+			{name: '.pdr1', type: 'padding-right:1rem '},
+			{name: '.pdr1-5', type: 'padding-right:1.5rem '},
+			{name: '.pdr2', type: 'padding-right:2rem '},
+			{name: '.pdr2-5', type: 'padding-right:2.5rem '},
+			{name: '.pdr3', type: 'padding-right:3rem '},
+			{name: '.pdr3-5', type: 'padding-right:3.5rem '},
+			{name: '.pdr4', type: 'padding-right:4rem '},
+			{name: '.pdr4-5', type: 'padding-right:4.5rem '},
+			{name: '.pdr5', type: 'padding-right:5rem '},
+			{name: '.pdr5-5', type: 'padding-right:5.5rem '},
+			{name: '.pdr6', type: 'padding-right:6rem '},
+			{name: '.pdr6-5', type: 'padding-right:6.5rem '},
+			{name: '.pdr7', type: 'padding-right:7rem '},
+			{name: '.pdr7-5', type: 'padding-right:7.5rem '},
+			{name: '.pdr8', type: 'padding-right:8rem '},
+			{name: '.pdr8-5', type: 'padding-right:8.5rem '},
+			{name: '.pdr9', type: 'padding-right:9rem '},
+			{name: '.pdr9-5', type: 'padding-right:9.5rem '}
+    ],
+    style_margin: [
+    	{name: '.mg-1', type: 'margin:.1rem' },
+			{name: '.mg-2', type: 'margin:.2rem' },
+			{name: '.mg-3', type: 'margin:.3rem' },
+			{name: '.mg-4', type: 'margin:.4rem' },
+			{name: '.mg-5', type: 'margin:.5rem' },
+			{name: '.mg-6', type: 'margin:.6rem' },
+			{name: '.mg-7', type: 'margin:.7rem' },
+			{name: '.mg-8', type: 'margin:.8rem' },
+			{name: '.mg-9', type: 'margin:.9rem' },
+			{name: '.mg1', type: 'margin:1rem' },
+			{name: '.mg1-5', type: 'margin:1.5rem' },
+			{name: '.mg2', type: 'margin:2rem' },
+			{name: '.mg2-5', type: 'margin:2.5rem' },
+			{name: '.mg3', type: 'margin:3rem' },
+			{name: '.mg3-5', type: 'margin:3.5rem' },
+			{name: '.mg4', type: 'margin:4rem' },
+			{name: '.mg4-5', type: 'margin:4.5rem' },
+			{name: '.mg5', type: 'margin:5rem' },
+			{name: '.mg5-5', type: 'margin:5.5rem' },
+			{name: '.mg6', type: 'margin:6rem' },
+			{name: '.mg6-5', type: 'margin:6.5rem' },
+			{name: '.mg7', type: 'margin:7rem' },
+			{name: '.mg7-5', type: 'margin:7.5rem' },
+			{name: '.mg8', type: 'margin:8rem' },
+			{name: '.mg8-5', type: 'margin:8.5rem' },
+			{name: '.mg9', type: 'margin:9rem' },
+			{name: '.mg9-5', type: 'margin:9.5rem' },
+
+			{name: '.mgb-1', type: 'margin-bottom:.1rem' },
+			{name: '.mgb-2', type: 'margin-bottom:.2rem' },
+			{name: '.mgb-3', type: 'margin-bottom:.3rem' },
+			{name: '.mgb-4', type: 'margin-bottom:.4rem' },
+			{name: '.mgb-5', type: 'margin-bottom:.5rem' },
+			{name: '.mgb-6', type: 'margin-bottom:.6rem' },
+			{name: '.mgb-7', type: 'margin-bottom:.7rem' },
+			{name: '.mgb-8', type: 'margin-bottom:.8rem' },
+			{name: '.mgb-9', type: 'margin-bottom:.9rem' },
+			{name: '.mgb1', type: 'margin-bottom:1rem' },
+			{name: '.mgb1-5', type: 'margin-bottom:1.5rem' },
+			{name: '.mgb2', type: 'margin-bottom:2rem' },
+			{name: '.mgb2-5', type: 'margin-bottom:2.5rem' },
+			{name: '.mgb3', type: 'margin-bottom:3rem' },
+			{name: '.mgb3-5', type: 'margin-bottom:3.5rem' },
+			{name: '.mgb4', type: 'margin-bottom:4rem' },
+			{name: '.mgb4-5', type: 'margin-bottom:4.5rem' },
+			{name: '.mgb5', type: 'margin-bottom:5rem' },
+			{name: '.mgb5-5', type: 'margin-bottom:5.5rem' },
+			{name: '.mgb6', type: 'margin-bottom:6rem' },
+			{name: '.mgb6-5', type: 'margin-bottom:6.5rem' },
+			{name: '.mgb7', type: 'margin-bottom:7rem' },
+			{name: '.mgb7-5', type: 'margin-bottom:7.5rem' },
+			{name: '.mgb8', type: 'margin-bottom:8rem' },
+			{name: '.mgb8-5', type: 'margin-bottom:8.5rem' },
+			{name: '.mgb9', type: 'margin-bottom:9rem' },
+			{name: '.mgb9-5', type: 'margin-bottom:9.5rem' },
+
+			{name: '.mgt-1', type: 'margin-top:.1rem' },
+			{name: '.mgt-2', type: 'margin-top:.2rem' },
+			{name: '.mgt-3', type: 'margin-top:.3rem' },
+			{name: '.mgt-4', type: 'margin-top:.4rem' },
+			{name: '.mgt-5', type: 'margin-top:.5rem' },
+			{name: '.mgt-6', type: 'margin-top:.6rem' },
+			{name: '.mgt-7', type: 'margin-top:.7rem' },
+			{name: '.mgt-8', type: 'margin-top:.8rem' },
+			{name: '.mgt-9', type: 'margin-top:.9rem' },
+			{name: '.mgt1', type: 'margin-top:1rem' },
+			{name: '.mgt1-5', type: 'margin-top:1.5rem' },
+			{name: '.mgt2', type: 'margin-top:2rem' },
+			{name: '.mgt2-5', type: 'margin-top:2.5rem' },
+			{name: '.mgt3', type: 'margin-top:3rem' },
+			{name: '.mgt3-5', type: 'margin-top:3.5rem' },
+			{name: '.mgt4', type: 'margin-top:4rem' },
+			{name: '.mgt4-5', type: 'margin-top:4.5rem' },
+			{name: '.mgt5', type: 'margin-top:5rem' },
+			{name: '.mgt5-5', type: 'margin-top:5.5rem' },
+			{name: '.mgt6', type: 'margin-top:6rem' },
+			{name: '.mgt6-5', type: 'margin-top:6.5rem' },
+			{name: '.mgt7', type: 'margin-top:7rem' },
+			{name: '.mgt7-5', type: 'margin-top:7.5rem' },
+			{name: '.mgt8', type: 'margin-top:8rem' },
+			{name: '.mgt8-5', type: 'margin-top:8.5rem' },
+			{name: '.mgt9', type: 'margin-top:9rem' },
+			{name: '.mgt9-5', type: 'margin-top:9.5rem' },
+
+			{name: '.mgl-1', type: 'margin-left:.1rem' },
+			{name: '.mgl-2', type: 'margin-left:.2rem' },
+			{name: '.mgl-3', type: 'margin-left:.3rem' },
+			{name: '.mgl-4', type: 'margin-left:.4rem' },
+			{name: '.mgl-5', type: 'margin-left:.5rem' },
+			{name: '.mgl-6', type: 'margin-left:.6rem' },
+			{name: '.mgl-7', type: 'margin-left:.7rem' },
+			{name: '.mgl-8', type: 'margin-left:.8rem' },
+			{name: '.mgl-9', type: 'margin-left:.9rem' },
+			{name: '.mgl1', type: 'margin-left:1rem' },
+			{name: '.mgl1-5', type: 'margin-left:1.5rem' },
+			{name: '.mgl2', type: 'margin-left:2rem' },
+			{name: '.mgl2-5', type: 'margin-left:2.5rem' },
+			{name: '.mgl3', type: 'margin-left:3rem' },
+			{name: '.mgl3-5', type: 'margin-left:3.5rem' },
+			{name: '.mgl4', type: 'margin-left:4rem' },
+			{name: '.mgl4-5', type: 'margin-left:4.5rem' },
+			{name: '.mgl5', type: 'margin-left:5rem' },
+			{name: '.mgl5-5', type: 'margin-left:5.5rem' },
+			{name: '.mgl6', type: 'margin-left:6rem' },
+			{name: '.mgl6-5', type: 'margin-left:6.5rem' },
+			{name: '.mgl7', type: 'margin-left:7rem' },
+			{name: '.mgl7-5', type: 'margin-left:7.5rem' },
+			{name: '.mgl8', type: 'margin-left:8rem' },
+			{name: '.mgl8-5', type: 'margin-left:8.5rem' },
+			{name: '.mgl9', type: 'margin-left:9rem' },
+			{name: '.mgl9-5', type: 'margin-left:9.5rem' },
+
+			{name: '.mgr-1', type: 'margin-right:.1rem' },
+			{name: '.mgr-2', type: 'margin-right:.2rem' },
+			{name: '.mgr-3', type: 'margin-right:.3rem' },
+			{name: '.mgr-4', type: 'margin-right:.4rem' },
+			{name: '.mgr-5', type: 'margin-right:.5rem' },
+			{name: '.mgr-6', type: 'margin-right:.6rem' },
+			{name: '.mgr-7', type: 'margin-right:.7rem' },
+			{name: '.mgr-8', type: 'margin-right:.8rem' },
+			{name: '.mgr-9', type: 'margin-right:.9rem' },
+			{name: '.mgr1', type: 'margin-right:1rem' },
+			{name: '.mgr1-5', type: 'margin-right:1.5rem' },
+			{name: '.mgr2', type: 'margin-right:2rem' },
+			{name: '.mgr2-5', type: 'margin-right:2.5rem' },
+			{name: '.mgr3', type: 'margin-right:3rem' },
+			{name: '.mgr3-5', type: 'margin-right:3.5rem' },
+			{name: '.mgr4', type: 'margin-right:4rem' },
+			{name: '.mgr4-5', type: 'margin-right:4.5rem' },
+			{name: '.mgr5', type: 'margin-right:5rem' },
+			{name: '.mgr5-5', type: 'margin-right:5.5rem' },
+			{name: '.mgr6', type: 'margin-right:6rem' },
+			{name: '.mgr6-5', type: 'margin-right:6.5rem' },
+			{name: '.mgr7', type: 'margin-right:7rem' },
+			{name: '.mgr7-5', type: 'margin-right:7.5rem' },
+			{name: '.mgr8', type: 'margin-right:8rem' },
+			{name: '.mgr8-5', type: 'margin-right:8.5rem' },
+			{name: '.mgr9', type: 'margin-right:9rem' },
+			{name: '.mgr9-5', type: 'margin-right:9.5rem' }
+    ],
+    style_border_radius: [
+    	{name: '.bdr', type: 'border-radius' },
+    	{name: '.bdr-lt', type: 'border-radius-left-top' },
+    	{name: '.bdr-lb', type: 'border-radius-left-bottom' },
+    	{name: '.bdr-rt', type: 'border-radius-right-top' },
+    	{name: '.bdr-rb', type: 'border-radius-right-bottom' }
+    ],
+    style_overflow: [
+    	{name: '.oh', type: 'overflow:hidden' },
+    ],
+    style_float: [
+    	{name: '.fl', type: 'float:left' },
+    	{name: '.fr', type: 'float:right' },
+    ],
+    style_font_color: [
+    	{name: '.fl', type: 'float:left' },
+    	{name: '.fl', type: 'float:left' },
+    	{name: '.fl', type: 'float:left' },
+    ],
+    style_font_color: [
+    ],
+    style_font_color: [
     ]
   }
 ];
 
-console.log(data);
